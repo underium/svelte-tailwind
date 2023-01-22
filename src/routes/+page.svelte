4b2665a6b51d1698da860b7601084f84d135388d
Javier Cáceres <4322278+underium@users.noreply.github.com>
2023-01-22 16:14:50 +0100

    added text-center to h1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <div class="flex flex-col items-center justify-center min-h-screen">
-	<h1 class="text-6xl font-bold underline">Svelte + Tailwind</h1>
+	<h1 class="text-center text-6xl font-bold underline">Svelte + Tailwind</h1>
 	<p class="my-4">It's working so hello world!</p>
 </div>
 
